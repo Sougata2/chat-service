57ebae745913e2614feb5a6b7983ca04c20f871f
sougata <sougata19582001@gmail.com>
2026-03-11 00:08:40 +0530

    added essential fields in room dto.

--- src/main/java/com/domain/chat_service/app/room/dto/RoomDto.java
+++ src/main/java/com/domain/chat_service/app/room/dto/RoomDto.java
@@ -1,7 +1,11 @@
 package com.domain.chat_service.app.room.dto;
 
+import com.domain.chat_service.app.room.enums.Type;
+import com.domain.chat_service.app.user.dto.UserInfo;
 import lombok.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -10,5 +14,11 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RoomDto {
+    private Long id;
     private UUID referenceNumber;
+    private Type type;
+    private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private List<UserInfo> participants;
 }
