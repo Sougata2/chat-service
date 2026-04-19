424bc6195bf8394c462aedd9b5a4ffad8e694cfe
sougata <sougata19582001@gmail.com>
2026-04-20 00:10:09 +0530

    added notification client and notification dto

---
+++ src/main/java/com/domain/chat_service/app/dto/NotificationDto.java
@@ -0,0 +1,19 @@
+package com.domain.chat_service.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationDto {
+    private List<String> emails;
+    private String title;
+    private String body;
+}
+
---
+++ src/main/java/com/domain/chat_service/client/notification/NotificationClient.java
@@ -0,0 +1,13 @@
+package com.domain.chat_service.client.notification;
+
+import com.domain.chat_service.app.dto.NotificationDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "notification-service")
+public interface NotificationClient {
+    @PostMapping("/web-push/notify")
+    void notify(@RequestHeader("X-Username") String username, @RequestHeader("X-Role") String role, @RequestBody NotificationDto dto);
+}
